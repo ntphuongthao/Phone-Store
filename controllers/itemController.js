291f27dda95bbfede10e24bfb5c0c1a07d92b638
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2022-11-18 14:31:11 -0500

    Sketched the layout of the index page

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,8 +1,32 @@
 const Item = require('../models/item');
+const Brand = require('../models/brand');
+const Category = require('../models/category');
 const async = require('async');
 
 exports.index = (req, res, next) => {
-    res.render("../views/item/index");
+    async.parallel(
+        {
+            brands(callback) {
+                Brand.find({}).sort({ name: 1}).exec(callback)
+            },
+            categories(callback) {
+                Category.find({}).sort({ name: 1}).exec(callback)
+            },
+            items(callback) {
+                Item.find({}).sort({ name: 1}).populate("brand").populate("category").exec(callback)
+            }
+        },
+        (err, results) => {
+            if (err) {
+                return next(err);
+            }
+            res.render('./item/index', {
+                brands: results.brands,
+                categories: results.categories,
+                items: results.items,
+            });
+        }
+    );
 }
 
 exports.item_list = (req, res, next) => {
