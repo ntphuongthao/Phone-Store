c1e62d4bd26686d84583c4f89bfb583896f3313d
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2022-11-20 01:19:26 -0500

    Added item_form successfully

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,6 +3,7 @@ const Brand = require('../models/brand');
 const Category = require('../models/category');
 const async = require('async');
 const mongoose = require('mongoose');
+const { body, check, validationResult } = require('express-validator');
 
 exports.index = (req, res, next) => {
     async.parallel(
@@ -66,12 +67,94 @@ exports.item_detail = (req, res, next) => {
 }
 
 exports.item_create_get = (req, res, next) => {
-    res.send("NOT IMPLEMENTED: item_create_get");
+    async.parallel(
+        {
+            brands(callback) {
+                Brand.find(callback);
+            },
+            categories(callback) {
+                Category.find(callback);
+            },
+        },
+        function(err, results) {
+            if (err) {
+                return next(err);
+            }
+            res.render("./item/item_form", {
+                title: "Create a new Book",
+                brands: results.brands,
+                categories: results.categories,
+            });
+        }
+    )
 }
 
-exports.item_create_post = (req, res, next) => {
-    res.send("NOT IMPLEMENTED: item_create_post");
-}
+exports.item_create_post = [
+    body("name", "Name of the item must be specified")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("brand", "Brand must be attached")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("description")
+        .trim()
+        .isLength({ min: 5 })
+        .escape(),
+    check("numberInStock")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    check("category.*").escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        const item = new Item({
+            name: req.body.name,
+            brand: req.body.brand,
+            category: req.body.category,
+            description: req.body.description,
+            price: req.body.price,
+            numberInStock: req.body.numberInStock,
+            image: req.body.image,
+        });
+
+        if (!errors.isEmpty()) {
+            async.parallel(
+                {
+                    brands(callback) {
+                        Brand.find(callback);
+                    },
+                    categories(callback) {
+                        Category.find(callback);
+                    }
+                },
+                (err, results) => {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    res.render("./item/item_form", {
+                        title: "Create a new Item",
+                        brands: results.brands,
+                        categories: results.categories,
+                        item,
+                        errors: errors.array(),
+                    });
+                }
+            );
+            return;
+        }
+
+        item.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            res.redirect(item.url);
+        });
+    }
+];
 
 exports.item_update_get = (req, res, next) => {
     res.send("NOT IMPLEMENTED: item_update_get");
